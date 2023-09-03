fa5f1cef2ba9f8fc445c39fb88e3892e3a6ea652
40gilad <40gilad.shenkar@gmail.com>
2023-09-03 12:59:03 +0300

    new mterials, prefabs and positions corrected

--- year_3/SS/ex2/Backgamoon/Assets/Scripts/SC_Board.cs
+++ year_3/SS/ex2/Backgamoon/Assets/Scripts/SC_Board.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SC_Board : MonoBehaviour
 {
     private float PieceStackDist = 0.7f;
-
     private void Awake()
     {
         GameObject.Find("Triangles").gameObject.SetActive(false);
+
     }
     // Start is called before the first frame update
     void Start()
