2ca08206e02f0b5ef3c4837466f9753a5ec85810
40gilad <40gilad.shenkar@gmail.com>
2023-09-03 15:51:11 +0300

    traignels init

--- year_3/SS/ex2/Backgamoon/Assets/Scripts/SC_Board.cs
+++ year_3/SS/ex2/Backgamoon/Assets/Scripts/SC_Board.cs
@@ -2,24 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-
 public class SC_Board : MonoBehaviour
 {
+    private static int TRIANGLES_AMOUNT = 24;
     private float PieceStackDist = 0.7f;
-    private void Awake()
+    Dictionary<string, GameObject> TrianglesContainers= new Dictionary<string, GameObject>();
+
+    void Awake()
     {
-        GameObject.Find("Triangles").gameObject.SetActive(false);
+        assign_values_to_TrianglesContainers();
 
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void assign_values_to_TrianglesContainers()
+    {
+        /* FUNCTION ALSO SETACTIVE FALSE TO ALL TRIANGLES */
+        string currname;
+        Transform curr_triangle_transform;
+        for (int i = 0; i < TRIANGLES_AMOUNT; i++)
+        {
+            currname = "Triangle" + i;
+            Debug.Log("Awake" + currname);
+            curr_triangle_transform = GameObject.Find(currname).transform;
+            Change_TriangleState(curr_triangle_transform.Find("Sprite_Triangle").gameObject);
+            TrianglesContainers.Add(currname, curr_triangle_transform.Find("TrianglePiecesStack").gameObject);
+        }
+    }
+    void Change_TriangleState(GameObject t)
+    {
+        t.SetActive(!t.activeSelf);
     }
 }
