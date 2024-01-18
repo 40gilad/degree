f4886139f3684c857072cc0c286cd2859d430a1a
GILAD MEIR <40gilad@gmail.com>
2024-01-18 09:45:22 +0200

    forced initial commit

---
+++ year_4/sm1/games_servers/class2/class2/Managers/EmployeeManager.cs
@@ -0,0 +1,40 @@
+﻿using class2.Models;
+namespace class2.Managers
+{
+    public class EmployeeManager
+    {
+        private Dictionary<int, Employee> emp_data;
+
+        #region Singleton
+        public static EmployeeManager _instance;
+        public static EmployeeManager Instance
+        {
+            get
+            {
+                if(_instance == null)
+                    _instance = new EmployeeManager();
+                return _instance;
+            }
+        }
+        #endregion
+        public EmployeeManager()
+        {
+            emp_data = new Dictionary<int,Employee>();
+        }
+
+        public bool AddEmployee(Employee emp)
+        {
+            if (emp_data==null)
+                emp_data = new Dictionary<int, Employee>();
+            if (emp == null) 
+                return false;
+            if (!emp_data.ContainsKey(emp.id))
+            {
+                emp_data.Add(emp_data.Id,)
+                return true;
+            }
+            else return false;
+        }
+    }
+
+}
---
+++ year_4/sm1/games_servers/class2/class2/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace class2.Models
+{
+    public class Employee
+    {
+        private int _id;
+        private string _name;
+        public int id { get { return _id; } }
+        public string name { get { return _name; } }
+
+        public Employee(int id, string name)
+        {
+            _id = id;
+            _name = name;
+        }
+    }
+}
