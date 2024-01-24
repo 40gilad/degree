355d2bf414f468648a5f982f241b6cf570a64adc
GILAD MEIR <40gilad@gmail.com>
2024-01-24 13:34:59 +0200

    32-bit Prime numbers generator

--- year_4/sm1/criptogaphy/hw4/pythonProject/cripto_hw4.py
+++ year_4/sm1/criptogaphy/hw4/pythonProject/cripto_hw4.py
@@ -1,6 +1,5 @@
 import random
 import math
-import ctypes
 
 
 # ------------------------ CRYPTO HW4 GILAD MEIR ------------------------#
@@ -68,9 +67,10 @@ def generate_odd_unsigned_long_long():
 def rabin_miller(num_to_check, rounds=20):
     """
     :param num_to_check: odd 32-bit integer
+    :param rounds: default 20
     :return: bool: True if number is probably prime, False if its surly isn't
     """
-    exp=num_to_check-1
+    exp = num_to_check - 1
     curr_round = rounds
     while rounds != 0:
         if 22 < num_to_check:
@@ -80,14 +80,14 @@ def rabin_miller(num_to_check, rounds=20):
         div = 1
         while curr_round != 0 and exp % 2 == 0:
             exp = int(exp / div)
-            div=2
-            ans = square_and_multiply(base=a,exponent=exp,mod=num_to_check)
+            div = 2
+            ans = square_and_multiply(base=a, exponent=exp, mod=num_to_check)
             if ans != 1 and (ans - num_to_check) != -1:
                 return (
                     False,
                     [
-                        euclidean_gcd(num_to_check, ans + 1)
-                        , euclidean_gcd(num_to_check, ans - 1)
+                        euclidean_gcd(num_to_check, ans + 1),
+                        euclidean_gcd(num_to_check, ans - 1)
                     ]
                 )
             curr_round -= 1
@@ -98,8 +98,39 @@ def rabin_miller(num_to_check, rounds=20):
 
 # endregion
 
+# region Question 3
+
+
+def generate_long_long_prime(trys=20):
+    counter = 1
+    while counter < trys:
+        num = generate_odd_unsigned_long_long()
+        is_prime, gcd = rabin_miller(num)
+        if is_prime:
+            return num, counter
+        counter += 1
+    return 0, counter
+
+
+# endregion
 
 if __name__ == "__main__":
     gcd = []
-    is_prime, gcd = rabin_miller(75)
-    kaki = 1
+    function_call_amount = 5  # run few times
+    success_round_sum = 0
+    success = 0
+    print('----------------------------------------------------------------')
+
+    for i in range(function_call_amount):
+        num, rounds = generate_long_long_prime()
+        print(f'RUN NUMBER {i}:')
+        if num != 0:
+            success += 1
+            success_round_sum += rounds
+            print(f'After {rounds} rounds, I found 32 bit prime: {num}')
+        else:
+            print(f"After {rounds} rounds, I didn't find 32 bit prime")
+
+        print('----------------------------------------------------------------')
+    print(f'\nOut of {function_call_amount}, I found:\n {success} prime numbers\n'
+          f'With an average rounds of:\n {(success_round_sum / function_call_amount)} per number')
