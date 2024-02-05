6f3abc117075c0e16df15e12fa46e7cf8ac8f9e6
GILAD MEIR <40gilad@gmail.com>
2024-02-05 15:35:19 +0200

    displays right knee degree

--- year_4/sm1/final_project/PoseDetect/MediaPipeProject/main.py
+++ year_4/sm1/final_project/PoseDetect/MediaPipeProject/main.py
@@ -9,6 +9,22 @@ import datetime
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
+import cv2
+
+
+def black_and_white(frame):
+    # Convert the frame to black and white
+    bw_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # If you want to keep it as a 3-channel image (BGR) with the same values for R, G, and B
+    bw_frame_bgr = cv2.cvtColor(bw_frame, cv2.COLOR_GRAY2BGR)
+
+    return bw_frame_bgr
+
+
+def smooth_image(image):
+    return cv2.GaussianBlur(image, (5, 5), 0)
+
 
 def sharpen_image(image):
     kernel = np.array([[-1, -1, -1],
@@ -17,64 +33,95 @@ def sharpen_image(image):
     return cv2.filter2D(image, -1, kernel)
 
 
-video_path = "C:\\Users\\40gil\\Desktop\\degree\\year_4\\sm1\\final_project\\PoseDetect\\videos\\SlowMotion.mp4"
-dir_name=f'RUN_{datetime.datetime.now().strftime("%d%m_%H%M")}'
-# VIDEO FEED
+def display_and_save_image(image, landmarks, connections, frame_number, dir_name):
+    output_folder = f'ImagesOutput\\Raw\\{dir_name}'
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-cap = cv2.VideoCapture(video_path)
-## Setup mediapipe instance
-with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.1) as pose:
-    counter=0
-    while cap.isOpened():
-        ret, frame = cap.read()
+    # Render detections
+    mp_drawing.draw_landmarks(image, landmarks, connections,
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
 
-        # Recolor image to RGB
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
+    # Add frame number to the image
+    frame_number_text = f"Frame: {frame_number}"
+    cv2.putText(image, frame_number_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.imshow('Mediapipe Feed', image)
+    cv2.imwrite(os.path.join(output_folder, f"SlowMo_frame{frame_number}.jpg"), image)
 
-        en_image = sharpen_image(sharpen_image(image))
 
-        # Make detection
-        results = pose.process(image)
-        output_folder = f'..\\ImagesOutput\\Raw\\{dir_name}'
+def calculate_angle(a, b, c):
+    a = np.array(a)  # First
+    b = np.array(b)  # Mid
+    c = np.array(c)  # End
 
-        # Make detection
-        en_results = pose.process(en_image)
-        en_output_folder = f'..\\ImagesOutput\\Enhanced\\{dir_name}'
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
 
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
+    if angle > 180.0:
+        angle = 360 - angle
 
-        if not os.path.exists(en_output_folder):
-            os.makedirs(en_output_folder)
+    return angle
 
-        # Recolor back to BGR
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+def main(video_path="C:\\Users\\40gil\\Desktop\\degree\\year_4\\sm1\\final_project\\PoseDetect\\videos\\SlowMotion.mp4"
+         ):
+    dir_name = f'RUN_{datetime.datetime.now().strftime("%d%m_%H%M")}'
+    # VIDEO FEED
 
-        # Recolor back to BGR
-        en_image.flags.writeable = True
-        en_image = cv2.cvtColor(en_image, cv2.COLOR_RGB2BGR)
+    cap = cv2.VideoCapture(video_path)
+    ## Setup mediapipe instance
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0, model_complexity=2) as pose:
+        counter = 0
+        while cap.isOpened():
+            ret, frame = cap.read()
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
 
-        # Render detections
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
-                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-                                  )
+            # Make detection
+            results = pose.process(image)
 
-        # Render detections
-        mp_drawing.draw_landmarks(en_image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
-                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-                                  )
-        cv2.imwrite(os.path.join(output_folder, f"SlowMo_frame{counter}.jpg"), image)
-        cv2.imwrite(os.path.join(en_output_folder, f"SlowMo_frame{counter}.jpg"), en_image)
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        counter += 1
+            # Extract landmarks
+            try:
+                landmarks = results.pose_landmarks.landmark
+            except:
+                pass
 
-        cv2.imshow('Mediapipe Feed', image)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+                #extractin x,y values for each joint
+            Lhip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+                        landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+            Lknee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
+                         landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+            Lankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
+                         landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+            angle=calculate_angle(Lhip,Lknee,Lankle)
+            disp_angle=format(angle,".2f")
+            # Visualize angle
+            cv2.putText(image, f'Right knee degree: {disp_angle}',
+                           (10,60),
+                           cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
+                                )
 
-cap.release()
-cv2.destroyAllWindows()
+            # --------------------- OUTPUTTING THE POSE TO SCREEN AND FILE ---------------------#
+
+            display_and_save_image(image=image, landmarks=results.pose_landmarks,
+                                   connections=mp_pose.POSE_CONNECTIONS,
+                                   frame_number=counter, dir_name=dir_name
+                                   )
+            counter += 1
+
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                # if 'q' button was pressed
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
