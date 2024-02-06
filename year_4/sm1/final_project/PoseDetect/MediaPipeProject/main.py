99905c170bae00a199ff251841a1bf76ea89d1dc
GILAD MEIR <40gilad@gmail.com>
2024-02-06 16:44:35 +0200

    structure changes

--- year_4/sm1/final_project/PoseDetect/MediaPipeProject/main.py
+++ year_4/sm1/final_project/PoseDetect/MediaPipeProject/main.py
@@ -1,17 +1,25 @@
-from datetime import time
-
-import cv2
 import mediapipe as mp
 import os
 import numpy as np
 import datetime
+import cv2
 
+# region Globals
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
-import cv2
 
+# endregion
 
+#region Support functions
+
+def create_output_folder(output_folder):
+    output_folder = f'FramesOutput\\{output_folder}'
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+#endregion
+
+# region Image Menipulation
 def black_and_white(frame):
     # Convert the frame to black and white
     bw_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -33,10 +41,12 @@ def sharpen_image(image):
     return cv2.filter2D(image, -1, kernel)
 
 
-def display_and_save_image(image, landmarks, connections, frame_number, dir_name,to_save=True):
-    output_folder = f'ImagesOutput\\Raw\\{dir_name}'
-    if to_save and not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+# endregion
+
+# region Frames Analyze
+def display_and_save_image(image, landmarks, connections, frame_number, dir_name, display_window_name="Mediapipe Feed",
+                           to_save=True):
+    output_folder = f'FramesOutput\\{dir_name}'
 
     # Render detections
     mp_drawing.draw_landmarks(image, landmarks, connections,
@@ -47,7 +57,7 @@ def display_and_save_image(image, landmarks, connections, frame_number, dir_name
     # Add frame number to the image
     frame_number_text = f"Frame: {frame_number}"
     cv2.putText(image, frame_number_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
-    cv2.imshow('Mediapipe Feed', image)
+    cv2.imshow(display_window_name, image)
     if to_save:
         cv2.imwrite(os.path.join(output_folder, f"SlowMo_frame{frame_number}.jpg"), image)
 
@@ -65,9 +75,17 @@ def calculate_angle(a, b, c):
 
     return angle
 
-def main(video_path="C:\\Users\\40gil\\Desktop\\degree\\year_4\\sm1\\final_project\\PoseDetect\\videos\\SlowMotion.mp4"
-         ):
-    dir_name = f'RUN_{datetime.datetime.now().strftime("%d%m_%H%M")}'
+
+# endregion
+
+
+# region Main
+def main(source_video_dir_path,video_name,to_save=False):
+    output_dir_name = f'{video_name}_{datetime.datetime.now().strftime("Date-%d.%m_Time-%H.%M")}'
+    video_path = f'{source_video_dir_path}\\{video_name}.mp4'
+    if to_save:
+        create_output_folder(output_folder=output_dir_name)
+
     # VIDEO FEED
 
     cap = cv2.VideoCapture(video_path)
@@ -93,50 +111,48 @@ def main(video_path="C:\\Users\\40gil\\Desktop\\degree\\year_4\\sm1\\final_proje
             except:
                 pass
 
-                #extractin x,y values for each joint
+                # extracting x,y values for each joint
             Lhip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
-                        landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+                    landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
             Lknee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
-                         landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+                     landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
             Lankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
-                         landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+                      landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
 
-            LsoulderPos=[format(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,".2f"),
-                        format(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y,".2f"),
-                        format(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].z,".2f")]
+            LsoulderPos = [format(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x, ".2f"),
+                           format(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y, ".2f"),
+                           format(landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].z, ".2f")]
 
-
-
-            angle=calculate_angle(Lhip,Lknee,Lankle)
-            disp_angle=format(angle,".2f")
+            angle = calculate_angle(Lhip, Lknee, Lankle)
+            disp_angle = format(angle, ".2f")
             # Visualize angle
             cv2.putText(image, f'Right knee degree: {disp_angle}',
-                           (10,60),
-                           cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
-                                )
+                        (10, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
+                        )
 
             cv2.putText(image, f'Left Shoulder Position x: {LsoulderPos[0]}',
-                           (10,100),
-                           cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
-                                )
+                        (10, 100),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
+                        )
 
             cv2.putText(image, f'                       y: {LsoulderPos[1]}',
-                           (10,130),
-                           cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
-                                )
+                        (10, 130),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
+                        )
 
             cv2.putText(image, f'                       z: {LsoulderPos[2]}',
-                           (10,160),
-                           cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
-                                )
-
+                        (10, 160),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
+                        )
 
             # --------------------- OUTPUTTING THE POSE TO SCREEN AND FILE ---------------------#
 
             display_and_save_image(image=image, landmarks=results.pose_landmarks,
                                    connections=mp_pose.POSE_CONNECTIONS,
-                                   frame_number=counter, dir_name=dir_name,
-                                   to_save=False
+                                   frame_number=counter, dir_name=output_dir_name,
+                                   display_window_name=video_name,
+                                   to_save=to_save
                                    )
             counter += 1
 
@@ -148,5 +164,8 @@ def main(video_path="C:\\Users\\40gil\\Desktop\\degree\\year_4\\sm1\\final_proje
     cv2.destroyAllWindows()
 
 
+# endregion
+
 if __name__ == '__main__':
-    main()
+    main(souce_video_dir_path="C:\\Users\\40gil\\Desktop\\degree\\year_4\\sm1\\final_project\\PoseDetect\\videos",
+         video_name="SlowMotion_Doron")
