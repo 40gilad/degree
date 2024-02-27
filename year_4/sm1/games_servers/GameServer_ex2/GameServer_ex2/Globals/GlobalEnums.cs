432e6e1421ae26ecf6707feac8b899513f68c709
GILAD MEIR <40gilad@gmail.com>
2024-02-27 20:20:17 +0200

    globals class

---
+++ year_4/sm1/games_servers/GameServer_ex2/GameServer_ex2/Globals/GlobalEnums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer_ex2.Globals
+{
+    internal class GlobalEnums
+    {
+        public enum CloseConnectionCode { Unknown= 100,DuplicateConnection=101};
+    }
+}
