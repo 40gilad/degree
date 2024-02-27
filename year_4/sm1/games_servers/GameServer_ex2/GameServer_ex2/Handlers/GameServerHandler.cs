2b097e272e3c3925da14568c4982d9ebb302ffc5
GILAD MEIR <40gilad@gmail.com>
2024-02-27 16:14:12 +0200

    initial game server

---
+++ year_4/sm1/games_servers/GameServer_ex2/GameServer_ex2/Handlers/GameServerHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace GameServer_ex2.Handlers
+{
+    internal class GameServerHandler:WebSocketBehavior
+    {
+        protected override void OnOpen()
+        {
+            Console.WriteLine("OnOpen " + ID);
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            Console.WriteLine("OnClose " + ID);
+
+        }
+
+        protected override void OnError(ErrorEventArgs e)
+        {
+            Console.WriteLine("OnError " + ID);
+
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Console.WriteLine("OnMessage " + ID);
+
+        }
+    }
+
+}
