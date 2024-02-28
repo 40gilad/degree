baee07bf254e38dc0f5f612f095b3093b7d9564f
GILAD MEIR <40gilad@gmail.com>
2024-02-28 22:57:57 +0200

    more modules to create matching with searching matching players thread

---
+++ year_4/sm1/games_servers/GameServer_ex2/GameServer_ex2/Managers/SearchingManager.cs
@@ -0,0 +1,78 @@
+﻿using GameServer_ex2.Models;
+using GameServer_ex2.Threads;
+using System.Collections.Generic;
+
+namespace GameServer_ex2.Managers
+{
+    internal class SearchingManager
+    {
+        private Dictionary<string, int> searching_dict;
+        private MatchingThread curr_matching_thread;
+
+        #region Singleton
+        private static SearchingManager instance;
+        public static SearchingManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SearchingManager();
+                return instance;
+            }
+        }
+        #endregion
+
+        SearchingManager()
+        {
+            searching_dict = new Dictionary<string, int>();
+            curr_matching_thread = new MatchingThread();
+            curr_matching_thread.StartThread();
+        }
+
+        public bool AddToSearchingDict(string uid,int rating)
+        {
+            if (searching_dict == null)
+                searching_dict = new Dictionary<string, int>();
+            if (uid == null)
+                return false;
+            if (searching_dict.ContainsKey(uid))
+            {
+                searching_dict[uid] = rating;
+                return true;
+            }
+            searching_dict.Add(uid,rating);
+            return true;
+        }
+
+        public bool RemoveFromSearchingList(string uid)
+        {
+            if (searching_dict == null)
+                searching_dict = new Dictionary<string, int>();
+            if (uid==null)
+                return false;
+            if (searching_dict.ContainsKey(uid))
+            {
+                searching_dict.Remove(uid);
+                return true;
+            }
+            else
+                return false;
+
+        }
+
+        public List<SearchingData> GetSearchingList()
+        {
+            List<SearchingData> searching_data=new List<SearchingData>();
+            foreach(string uid in searching_dict.Keys)
+            {
+                searching_data.Add
+                    (new SearchingData(
+                        _uid: uid,
+                        rating: searching_dict[uid]
+                        )
+                    );
+            }
+            return searching_data;
+        }
+    }
+}
---
+++ year_4/sm1/games_servers/GameServer_ex2/GameServer_ex2/Threads/MatchingThread.cs
@@ -0,0 +1,113 @@
+﻿using GameServer_ex2.Managers;
+using GameServer_ex2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GameServer_ex2.Threads
+{
+    internal class MatchingThread
+    {
+        private Thread curr_thread;
+        private bool is_mathing_run = false;
+        private int gap_to_match = 50;
+        public MatchingThread()
+        {
+        }
+
+        public bool StartThread()
+        {
+            is_mathing_run = true;
+            curr_thread = new Thread(new ThreadStart(RunMatching));
+            curr_thread.Start();
+            return true;
+        }
+
+        public bool StartThread(string uid)
+        {
+            is_mathing_run = true;
+            //curr_thread = new Thread(new ThreadStart(RunMatching));
+            return true;
+        }
+
+        public void RunMatching()
+        {
+            while (is_mathing_run)
+            {
+                Console.WriteLine("\nthread run ");
+                MatchPlayers();
+                Thread.Sleep(1000); // wait 1000 miliseconds= 1 second
+            }
+        }
+
+        private void MatchPlayers()
+        {
+            bool is_found_match = false;
+            List<SearchingData> sorted_searching_list = SearchingManager.Instance.GetSearchingList().
+                OrderBy(value => value.PlayerRating).ToList();
+
+            if (sorted_searching_list != null && sorted_searching_list.Count > 0)
+            {
+                for (int i = 0;
+                    i < sorted_searching_list.Count;
+                    i++)
+                {
+                    SearchingData first = sorted_searching_list[i];
+                    for (int j = i + 1;
+                        j < sorted_searching_list.Count && !is_found_match;
+                        j++)
+                    {
+                        SearchingData second = sorted_searching_list[j];
+                        if (AreMatching(first_rating: first.PlayerRating, second_rating: second.PlayerRating))
+                        {
+                            List<User> matched_users = new List<User>()
+                            {
+                                SessionsManager.Instance.UserSession[first.UserID],
+                                SessionsManager.Instance.UserSession[second.UserID]
+
+                            };
+                            bool is_valid_users=CheckUsersValidity(matched_users);
+                        }
+
+                    }
+
+                }
+            }
+        }
+
+        private bool AreMatching(int first_rating, int second_rating)
+        {
+            int curr_gap = Math.Abs(first_rating - second_rating);//gap between players rating
+
+            if (curr_gap <= gap_to_match)
+                return true;
+            return false;
+        }
+
+        private bool CheckUsersValidity(List<User> users)
+        {
+            foreach (User user in users)
+            {
+                if (!IsValidUser(user))
+                    return true;
+            }
+            return false;
+        }
+        private bool IsValidUser(User user)
+        {
+            if (user != null &&
+                user.CurrState == User.UserState.Matching &&
+                user.IsUserLiveWithSession())
+                return true;
+            return false;
+        }
+
+        public void StopThread()
+        {
+            is_mathing_run = false;
+        }
+    }
+}
