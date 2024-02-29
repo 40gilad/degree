7e0a7479817ca71816dff42d1fd7b5d43345c849
GILAD MEIR <40gilad@gmail.com>
2024-02-29 15:15:15 +0200

    new class to use in matching thread

---
+++ year_4/sm1/games_servers/GameServer_ex2/GameServer_ex2/Managers/MatchingManager.cs
@@ -0,0 +1,82 @@
+﻿using GameServer_ex2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer_ex2.Managers
+{
+    internal class MatchingManager
+    {
+        private Dictionary<string, MatchData> all_matchings;
+
+        #region Singleton
+        private static MatchingManager instance;
+        public static MatchingManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new MatchingManager();
+                return instance;
+            }
+        }
+        #endregion
+
+        public MatchingManager()
+        {
+            if (all_matchings == null)
+                all_matchings = new Dictionary<string, MatchData>();
+        }
+
+        public bool AddMatching(MatchData match)
+        {
+            string match_id = match.MatchId.ToString();
+            if (all_matchings == null)
+                all_matchings = new Dictionary<string, MatchData>();
+
+            if (match == null)
+                return false;
+
+            if (all_matchings.ContainsKey(match_id))//all_matching already has this match_id
+            {
+                all_matchings[match_id] = match;
+                return true;
+            }
+
+            all_matchings.Add(match_id, match);
+            return true;
+        }
+
+        public bool RemoveMatching(MatchData match)
+        {
+            string match_id = match.MatchId.ToString();
+
+            if (all_matchings == null)
+                return true;
+
+            if (match == null)
+                return false;
+
+            if (all_matchings.ContainsKey(match_id))
+               all_matchings.Remove(match_id);
+
+            return true;
+        }
+
+        public MatchData GetMatchingData(int _match_id)
+        {
+            string match_id=_match_id.ToString();
+            if (
+                all_matchings != null &&
+                all_matchings.ContainsKey(match_id)
+                )
+                return all_matchings[match_id];
+            return null;
+
+        }
+
+    }
+
+}
---
+++ year_4/sm1/games_servers/GameServer_ex2/GameServer_ex2/Models/MatchData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer_ex2.Models
+{
+    internal class MatchData
+    {
+        private Dictionary<string, SearchingData> matched_players_data;//will hold {user_id : searching_data}
+        public Dictionary<string, SearchingData> MatchedPlayersData { get => matched_players_data;}
+
+        private int match_id;
+        public int MatchId { get => match_id; }
+
+        private string match_time;
+        public string MatchTime { get { return match_time; } }
+
+
+        public MatchData(List<SearchingData> search_data,int curr_match_id)
+        {
+            //MatchedPlayersData=......
+            match_id=curr_match_id;
+            match_time = DateTime.UtcNow.ToString();
+            foreach (SearchingData sd in search_data)
+            {
+                sd.IsPlayerReady = false;
+                matched_players_data.Add(sd.UserID, sd);
+            }
+
+
+        }
+    }
+}
