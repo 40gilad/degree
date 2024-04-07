52a4da525db457c6be8345002b8a610b80ad943b
GILAD MEIR <40gilad@gmail.com>
2024-04-08 01:29:55 +0300

    not throwing eeptions at client. need to check why client opens new room

--- year_4/sm1/games_servers/final_script/GameServerShenkar/GameServerShenkar/Requests/LiveRoomInfoRequest.cs
+++ year_4/sm1/games_servers/final_script/GameServerShenkar/GameServerShenkar/Requests/LiveRoomInfoRequest.cs
@@ -12,23 +12,42 @@ namespace GameServerShenkar.Requests
 {
     internal class LiveRoomInfoRequest
     {
+        public static string RoomPropertiesPassword = "Shenkar";
         public static Dictionary<string, object> Get(User CurUser, Dictionary<string, object> Details)
         {
             Dictionary<string, object> response = new Dictionary<string, object>();
+            Dictionary<string, object> prop = new Dictionary<string, object>();
             if (Details.ContainsKey("RoomId"))
             {
                 response.Add("Response", "GetLiveRoomInfo");
-                response.Add("RoomData", "GetLiveRoomInfo");
-                response.Add("RoomProperties", "GetLiveRoomInfo");
-                response.Add("Users", "GetLiveRoomInfo");
-
+                
                 // Users list
-                GameThread room= RoomsManager.Instance.GetRoomByRoomId(Details["RoomId"].ToString());
+                GameThread room = RoomsManager.Instance.GetRoomByRoomId(Details["RoomId"].ToString());
                 Dictionary<string, User> uid_user_pair = room.Users;
                 List<string> users = new List<string>();
                 foreach (KeyValuePair<string,User> pair in uid_user_pair)
                     users.Add(pair.Key);
-              
+                response.Add("Users", users);
+
+                //Room properties
+                prop["Password"] = RoomPropertiesPassword;
+                response.Add("RoomProperties", prop);
+
+                //Room Data
+                if (room != null)
+                {
+                    Dictionary<string, object> roomDetails = new Dictionary<string, object>();
+                    roomDetails.Add("RoomId", room.RoomId);
+                    roomDetails.Add("Name", room.RoomName);
+                    roomDetails.Add("TurnTime", room.TurnTime);
+                    roomDetails.Add("Owner", room.RoomOwner);
+                    roomDetails.Add("MaxUsersCount", room.MaxUsersCount);
+                    roomDetails.Add("JoindUserCount", room.JoindUserCount);
+                    response.Add("RoomData", roomDetails);
+                }
+                else
+                    response.Add("RoomData", null);
+
             }
             else
                 response.Add("Response", "GetLiveRoomInfo");
