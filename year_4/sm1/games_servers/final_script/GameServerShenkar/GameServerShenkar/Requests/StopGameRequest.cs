ccc35806e44d0cd6fb1228c30b8b7a9314187a8f
GILAD MEIR <40gilad@gmail.com>
2024-04-09 15:03:14 +0300

    end game- stop game request

---
+++ year_4/sm1/games_servers/final_script/GameServerShenkar/GameServerShenkar/Requests/StopGameRequest.cs
@@ -0,0 +1,28 @@
+﻿using GameServerShenkar.Managers;
+using GameServerShenkar.Models;
+using GameServerShenkar.Threads;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServerShenkar.Requests
+{
+    internal class StopGameRequest
+    {
+        public static Dictionary<string, object> Get(User CurUser, Dictionary<string, object> Details)
+        {
+            GameThread room = RoomsManager.Instance.GetRoomByMatchId(CurUser.MatchId);
+            Dictionary<string, object> response = new Dictionary<string, object>();
+            if (Details.ContainsKey("Winner"))
+            {
+                string winner = Details["Winner"].ToString();
+                if (winner != "Tie")
+                    room.SendChat(CurUser,"Winner is: \n"+winner);
+            }
+            
+            return response;
+        }
+    }
+}
