c873397f9eb8543ab97054f50571d05f7d718734
GILAD MEIR <40gilad@gmail.com>
2024-04-09 12:36:28 +0300

    game started in both clients

--- year_4/sm1/games_servers/final_script/GameServerShenkar/GameServerShenkar/Requests/StartGameRequest.cs
+++ year_4/sm1/games_servers/final_script/GameServerShenkar/GameServerShenkar/Requests/StartGameRequest.cs
@@ -1,4 +1,7 @@
-﻿using GameServerShenkar.Models;
+﻿using GameServerShenkar.Managers;
+using GameServerShenkar.Models;
+using GameServerShenkar.Threads;
+using GameServerShenkar.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +14,24 @@ namespace GameServerShenkar.Requests
     {
         public static Dictionary<string, object> Get(User CurUser, Dictionary<string, object> Details)
         {
+            string match_id = CurUser.MatchId;
+            GameThread curr = RoomsManager.Instance.GetRoomByMatchId(match_id); ;
+
+            // { "Service", "StartGame" }
             Dictionary<string, object> response = new Dictionary<string, object>();
-            return null;
+            List<string> PlayersList = new List<string>()
+            {
+                curr.RoomOwner,
+                curr.SecondPlayer
+            };
+            response.Add("Service", "StartGame");
+            response.Add("Sender", CurUser.UserId);
+            response.Add("RoomId", curr.RoomId);
+            response.Add("NextTurn", CurUser.UserId);
+            response.Add("TurnsList", PlayersList);
+            response.Add("TurnTime", curr.TurnTime);
+            curr.Users[curr.SecondPlayer].SendMessage(JsonLogic.Serialize(response));
+            return response;
         }
-        }
+    }
 }
