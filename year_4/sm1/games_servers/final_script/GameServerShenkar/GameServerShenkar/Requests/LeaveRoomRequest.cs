37dbcfe68548cb4c24053fb3097810f64fc85ad2
GILAD MEIR <40gilad@gmail.com>
2024-04-09 16:37:44 +0300

    minor changes

--- year_4/sm1/games_servers/final_script/GameServerShenkar/GameServerShenkar/Requests/LeaveRoomRequest.cs
+++ year_4/sm1/games_servers/final_script/GameServerShenkar/GameServerShenkar/Requests/LeaveRoomRequest.cs
@@ -13,16 +13,16 @@ namespace GameServerShenkar.Requests
     {
         public static Dictionary<string, object> Get(User CurUser, Dictionary<string, object> Details)
         {
-            /*
-             *             { "Service", "LeaveRoom"},
-                            { "RoomId", roomId }
-            */
+
             Dictionary<string, object> response = new Dictionary<string, object>();
             GameThread room = RoomsManager.Instance.GetRoomByMatchId(CurUser.MatchId);
             string CurUserId= CurUser.UserId;
             response.Add("Response", "LeaveRoom");
             if (Details.ContainsKey("RoomId"))
             {
+                if (CurUser.CurUserState == User.UserState.PrePlay)
+                    CurUser.CurUserState = User.UserState.Matching;
+
                 response.Add("RoomId", room.RoomId);
                 response.Add("MaxUsers", room.MaxUsersCount.ToString());
                 response.Add("IsSuccess", "true");
