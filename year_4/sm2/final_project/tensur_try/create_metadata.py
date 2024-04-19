9e872ddf96dfd4817abf476791e5a5f6eee0ae31
GILAD MEIR <40gilad@gmail.com>
2024-04-19 20:10:37 +0300

    added images to test

--- year_4/sm2/final_project/tensur_try/create_metadata.py
+++ year_4/sm2/final_project/tensur_try/create_metadata.py
@@ -1,13 +1,135 @@
 import pandas as pd
 import os
+import numpy as np
+from pathlib import Path
+from datetime import datetime
+
+
+# region Mediapipe
+import mediapipe as mp
+import cv2
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.8)
+
+
+def crop_images(path=None, size=(128, 128)):
+    # Create the new directory path
+    if path is None:
+        new_dir = Path(
+            r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\cropped_' +
+            str(size)
+            .replace('(', '')
+            .replace(',', 'X')
+            .replace(' ', '') + datetime.now().strftime("%H_%M_%S")
+        )
+    else:
+        new_dir = Path(path)
+
+    # Create the new directory if it doesn't exist
+    new_dir.mkdir(parents=True, exist_ok=True)
+
+    # Path to the original images directory
+    original_images_dir = Path(r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\pre_cropped_images')
+
+    # Loop through all directories in the original images directory
+    for dir_name in os.listdir(original_images_dir):
+        if dir_name == 'asl_alphabet_test':
+            for img_name in os.listdir(original_images_dir / dir_name):
+                print(f'-{img_name}')
+                img_path = original_images_dir /dir_name / img_name
+
+                this_path=new_dir / dir_name
+                this_path.mkdir(parents=True, exist_ok=True)
+
+                # Load and crop the image
+                img = cv2.imread(str(img_path))
+                if img is not None:
+                    cropped_img = cut_image(img, size)
+
+                    # Save the cropped image to the new location
+                    new_img_path = this_path / img_name
+                    cv2.imwrite(str(new_img_path), cropped_img)
+            continue
+        #else: continue
+        print(f'--------------{dir_name}')
+        dir_path = original_images_dir / dir_name
+        if os.path.isdir(dir_path):
+            new_subdir = new_dir / dir_name
+            new_subdir.mkdir(parents=True, exist_ok=True)
+
+            # Loop through all subdirectories in the current directory
+            for subdir_name in os.listdir(dir_path):
+                print(f'------{subdir_name}')
+                subdir_path = dir_path / subdir_name
+                if os.path.isdir(subdir_path):
+                    new_subsubdir = new_subdir / subdir_name
+                    new_subsubdir.mkdir(parents=True, exist_ok=True)
+
+                    # Loop through all images in the current subdirectory
+                    for img_name in os.listdir(subdir_path):
+                        print(f'-{img_name}')
+                        img_path = subdir_path / img_name
+
+                        # Load and crop the image
+                        img = cv2.imread(str(img_path))
+                        if img is not None:
+                            cropped_img = cut_image(img, size)
+
+                            # Save the cropped image to the new location
+                            new_img_path = new_subsubdir / img_name
+                            cv2.imwrite(str(new_img_path), cropped_img)
+
+
+def cut_image(image, size=(128, 128)):
+    """
+    :param image: cv2 image
+    :return: cv2 image (hand)
+    """
+    image = image.astype(np.uint8)
+    processed_image = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    if processed_image.multi_hand_landmarks:
+        hand_landmarks = processed_image.multi_hand_landmarks[0]
+        x_coords = [landmark.x for landmark in hand_landmarks.landmark]
+        y_coords = [landmark.y for landmark in hand_landmarks.landmark]
+        x_min, x_max = min(x_coords), max(x_coords)
+        y_min, y_max = min(y_coords), max(y_coords)
+
+        x_min_adjust = int(x_min * image.shape[1] - 80)
+        y_min_adjust = int(y_min * image.shape[0] - 80)
+        x_max_adjust = int(x_max * image.shape[1] + 80)
+        y_max_adjust = int(y_max * image.shape[0] + 80)
+        if x_min_adjust < 0:
+            x_min_adjust = int(x_min * image.shape[1] - 15)
+            if x_min_adjust < 0:
+                x_min_adjust = 0
+        if y_min_adjust < 0:
+            y_min_adjust = int(y_min * image.shape[0] - 15)
+            if y_min_adjust < 0:
+                y_min_adjust = 0
+        x_min = x_min_adjust
+        y_min = y_min_adjust
+        x_max = x_max_adjust
+        y_max = y_max_adjust
+        hand_region = image[y_min:y_max, x_min:x_max]
+        hand_region_uint8 = hand_region.astype(np.uint8)
+    else:
+        hand_region_uint8 = image.astype(np.uint8)
+    hand_region_bgr = cv2.cvtColor(hand_region_uint8, cv2.COLOR_RGB2BGR)
+    hand_region_bgr = cv2.resize(hand_region_bgr, dsize=size)
+    return hand_region_bgr
+
+
+# endregion
 
 
 def create_train_meta_data():
     df = pd.DataFrame()
     images_list = []
     labels_list = []
-    images_dir = r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\asl_alphabet_train\asl_alphabet_train'
+    images_dir = r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\cropped_128X128)18_57_14\asl_alphabet_train'
     class_encoding = {}
+    weights = []
     lable_class_encoding = []
     class_counter = 0
     for dir in os.listdir(images_dir):
@@ -16,13 +138,38 @@ def create_train_meta_data():
         class_encoding[dir] = class_counter
         class_counter += 1
         for img in os.listdir(os.path.join(images_dir, dir)):
-            images_list.append(os.path.join(dir, img))
+            images_list.append(os.path.join(images_dir, dir, img))
             labels_list.append(dir)
             lable_class_encoding.append(class_encoding[dir])
+            weights.append(1)
     df["filename"] = images_list
     df["label"] = labels_list
-    df['class_encoding']=lable_class_encoding
-    df.to_csv(os.path.join(images_dir, 'metadata.csv'), index=False)
+    df['class_encoding'] = lable_class_encoding
+    df['weights'] = weights
+
+    # ----------- df2 ---------------#
+    df2 = pd.DataFrame()
+    images_list = []
+    labels_list = []
+    weights = []
+    images_dir = r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\cropped_128X128)18_57_14\arabic_to_english'
+    lable_class_encoding = []
+    for dir in os.listdir(images_dir):
+        if len(dir) > 1:
+            continue
+        for img in os.listdir(os.path.join(images_dir, dir)):
+            images_list.append(os.path.join(images_dir, dir, img))
+            labels_list.append(dir)
+            lable_class_encoding.append(class_encoding[dir])
+            weights.append(10)
+    df2["filename"] = images_list
+    df2["label"] = labels_list
+    df2['class_encoding'] = lable_class_encoding
+    df2['weights'] = weights
+
+    final_df = pd.concat([df, df2], ignore_index=True)
+    final_df.to_csv(os.path.join(images_dir, 'metadata.csv'), index=False)
+    # df.to_csv(os.path.join(images_dir, 'metadata.csv'), index=False)
     return class_encoding
 
 
@@ -31,19 +178,40 @@ def create_test_meta_data(class_encoding):
     images_list = []
     labels_list = []
     lable_class_encoding = []
-    images_dir = r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\asl_alphabet_test\asl_alphabet_test'
+    images_dir = r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\cropped_128X128)18_57_14\asl_alphabet_test'
     for img in os.listdir(images_dir):
         cur_label = img.split('_')[0]
         if len(cur_label) > 1:
             continue
-        images_list.append(img)
+        images_list.append(os.path.join(images_dir, img))
         labels_list.append(cur_label)
         lable_class_encoding.append(class_encoding[cur_label])
 
     df["filename"] = images_list
     df["label"] = labels_list
-    df['class_encoding']=lable_class_encoding
-    df.to_csv(os.path.join(images_dir, 'metadata.csv'), index=False)
+    df['class_encoding'] = lable_class_encoding
+
+    df2 = pd.DataFrame()
+    images_list = []
+    labels_list = []
+    lable_class_encoding = []
+    images_dir = r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\cropped_128X128)18_57_14\hebrew_to_english'
+    for dir in os.listdir(images_dir):
+        if len(dir) > 1:
+            continue
+        for img in os.listdir(os.path.join(images_dir, dir)):
+            cur_label = img[0]
+            images_list.append(os.path.join(images_dir, dir, img))
+            labels_list.append(cur_label)
+            lable_class_encoding.append(class_encoding[cur_label])
+
+    df2["filename"] = images_list
+    df2["label"] = labels_list
+    df2['class_encoding'] = lable_class_encoding
+
+    final_df = pd.concat([df, df2], ignore_index=True)
+    final_df.to_csv(os.path.join(images_dir, 'metadata.csv'), index=False)
+    # df.to_csv(os.path.join(images_dir, 'metadata.csv'), index=False)
 
 
 def create_test_meta_data_2(class_encoding):
@@ -65,7 +233,10 @@ def create_test_meta_data_2(class_encoding):
     df["label"] = labels_list
     df['class_encoding'] = lable_class_encoding
     df.to_csv(os.path.join(images_dir, 'metadata.csv'), index=False)
+
+
 if __name__ == '__main__':
-    class_encoding=create_train_meta_data()
-    # create_test_meta_data(class_encoding)
+    #crop_images()
+    class_encoding = create_train_meta_data()
+    create_test_meta_data(class_encoding)
     create_test_meta_data_2(class_encoding)
--- year_4/sm2/final_project/tensur_try/pythonProject/main.py
+++ year_4/sm2/final_project/tensur_try/pythonProject/main.py
@@ -40,38 +40,43 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
+    # cut_image(cv2.imread(r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\images\hebrew_to_english\B\B17.jpeg'))
     project_name = 'letters'
     dataset_name = 'aaa'
 
     # -- paths ----
     train_path = Path(
-        r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\asl_alphabet_train\asl_alphabet_train')  # path to train images directory
+        r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\cropped_128X128)18_57_14\arabic_to_english')  # path to train images directory
     train_metapath = train_path / 'metadata.csv'
     test_path = Path(
-        r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\asl_alphabet_test\asl_alphabet_test')  # path to test images directory
+        r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\cropped_128X128)18_57_14\hebrew_to_english')  # path to test images directory
     test_metapath = test_path / 'metadata.csv'
-    test_2_path = Path(r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\arabic_to_english')
-    test_2_metapath =test_2_path/'metadata.csv'
+    test_2_path = Path(r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\images\hebrew_to_english')
+    test_2_metapath = test_2_path / 'metadata.csv'
     ########### TO LOAD MODEL AND TO RUN IT ON TEST SET
     loaded_model = False
-    loaded_model_dir = r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\firstRealRun_bugfix_100Epochs_20_52_03\firstRealRun_bugfix_100Epochs.h5'
+    loaded_model_dir = r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\running_outputs\bs=32_ts=(128, 128)_valSplit=0.2_lr=0.001_epochs=120_10_51_04\bs=32_ts=(128, 128)_valSplit=0.2_lr=0.001_epochs=120.h5'
     ####################
 
     # -- params ----
     bs = 32  # batch size
-    ts = (200, 200)  # target size
+    ts = (128, 128)  # target size
     x_col = 'filename'  # the column in the dataframe that contains the path to the images
     y_col = 'class_encoding'
     validation_split = 0.2  # train validation split
     lr = 1e-3
     epochs = 120
     steps = 100
-
-    tag = 'firstRealRun_horizontal_flip_120Epochs'  # run tag
+    if not loaded_model:
+        tag = f'bs={bs}_ts={ts}_valSplit={validation_split}_lr={lr}_epochs={epochs}'  # run tag
+    elif loaded_model:
+        test_paths = str(test_2_path).split('\\')
+        test_name = test_paths[len(test_paths) - 1]
+        tag = f'LoadedModel_tests={test_name}'  # run tag
 
     # --- dirs ---
     tagdir = Path(
-        r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project') / f'{tag}_{datetime.now().strftime("%H_%M_%S")}'
+        r'C:\Users\40gil\Desktop\degree\year_4\sm2\final_project\running_outputs') / f'{tag}_DateTime={datetime.now().strftime("%H_%M_%S")}'
     tagdir.mkdir(exist_ok=True, parents=True)
     plotsdir = tagdir / 'plots'
     plotsdir.mkdir(exist_ok=True, parents=True)
@@ -80,32 +85,43 @@ if __name__ == '__main__':
     df_trn = pd.read_csv(train_metapath)  # get metadata
     df_tst = pd.read_csv(test_metapath)  # get metadata
     df_tst_2 = pd.read_csv(test_2_metapath)  # get metadata
-    df_trn['filename'] = df_trn.filename.apply(lambda f: str(train_path / f))
+    # df_trn['filename'] = df_trn.filename.apply(lambda f: str(train_path / f))
     df_trn.class_encoding = df_trn.class_encoding
     df_trn['class_encoding'] = df_trn['class_encoding'].apply(lambda x: [x])
-    df_tst['filename'] = df_tst.filename.apply(lambda f: str(test_path / f))
+    # df_tst['filename'] = df_tst.filename.apply(lambda f: str(test_path / f))
     df_tst['class_encoding'] = df_tst['class_encoding'].apply(lambda x: [x])
-    df_tst_2['filename'] = df_tst_2.filename.apply(lambda f: str(test_2_path / f))
+    # df_tst_2['filename'] = df_tst_2.filename.apply(lambda f: str(test_2_path / f))
     df_tst_2['class_encoding'] = df_tst_2['class_encoding'].apply(lambda x: [x])
     df_tst_pp = df_tst.copy()
     df_tst_2_pp = df_tst_2.copy()
     df_trn_pp, df_val_pp = train_test_split(df_trn, test_size=validation_split,
                                             random_state=666)
 
+
     # --- make gens ----
-    trn_gen = ImageDataGenerator(rotation_range=10, width_shift_range=5, height_shift_range=5, zoom_range=0.1,
-                                 channel_shift_range=10, shear_range=10, horizontal_flip=True).flow_from_dataframe(df_trn_pp,
-                                                                                             x_col='filename',
-                                                                                             y_col='class_encoding',
-                                                                                             target_size=ts,
-                                                                                             class_mode='categorical',
-                                                                                             shuffle=True)
-    val_gen = ImageDataGenerator().flow_from_dataframe(df_val_pp, x_col='filename', y_col='class_encoding',
-                                                       target_size=ts, shuffle=False, class_mode='categorical')
-    tst_gen = ImageDataGenerator().flow_from_dataframe(df_tst_pp, x_col='filename', y_col='class_encoding',
-                                                       target_size=ts, shuffle=False, class_mode='categorical')
-    tst_2_gen = ImageDataGenerator().flow_from_dataframe(df_tst_2_pp, x_col='filename', y_col='class_encoding',
-                                                       target_size=ts, shuffle=False, class_mode='categorical')
+    trn_gen = ImageDataGenerator( rotation_range=10, width_shift_range=5,
+                                 height_shift_range=5, zoom_range=0.1,
+                                 channel_shift_range=10, shear_range=10, horizontal_flip=True).flow_from_dataframe(
+        df_trn_pp,
+        x_col='filename',
+        y_col='class_encoding',
+        weight_col='weights',
+        target_size=ts,
+        class_mode='categorical',
+        shuffle=True)
+    val_gen = ImageDataGenerator().flow_from_dataframe(df_val_pp, x_col='filename',
+                                                                                       y_col='class_encoding',
+                                                                                       weight_col='weights',
+                                                                                       target_size=ts, shuffle=False,
+                                                                                       class_mode='categorical')
+    tst_gen = ImageDataGenerator().flow_from_dataframe(df_tst_pp, x_col='filename',
+                                                                                       y_col='class_encoding',
+                                                                                       target_size=ts, shuffle=False,
+                                                                                       class_mode='categorical')
+    tst_2_gen = ImageDataGenerator().flow_from_dataframe(df_tst_2_pp, x_col='filename',
+                                                                                         y_col='class_encoding',
+                                                                                         target_size=ts, shuffle=False,
+                                                                                         class_mode='categorical')
 
     fit_dict = {
         'epochs': epochs,
@@ -129,8 +145,8 @@ if __name__ == '__main__':
     fn = tagdir / f'{tag}.h5'
     model.save(fn)
     class_encoding_revers = np.sort(df_trn.label.unique())
-    class_encoding_dict = dict(zip(np.arange(26),class_encoding_revers))
-    ticks_list = [str(ii)+'('+jj+')' for ii,jj in class_encoding_dict.items()]
+    class_encoding_dict = dict(zip(np.arange(26), class_encoding_revers))
+    ticks_list = [str(ii) + '(' + jj + ')' for ii, jj in class_encoding_dict.items()]
     print(f'Model saved to : {fn}')
     val_pred_raw = model.predict(val_gen)
     val_pred = val_pred_raw.argmax(axis=1)
@@ -141,17 +157,17 @@ if __name__ == '__main__':
     fig = plt.figure(figsize=(16, 16))
     ax = fig.add_subplot(111)
     sns.heatmap(conf_mat, annot=True, ax=ax)
-    ax.set_xticklabels(ticks_list,rotation=45)
-    ax.set_yticklabels(ticks_list,rotation=0)
-    ax.tick_params(axis='both', which='major',labelsize=10)
+    ax.set_xticklabels(ticks_list, rotation=45)
+    ax.set_yticklabels(ticks_list, rotation=0)
+    ax.tick_params(axis='both', which='major', labelsize=10)
     plt.title(f'acc={val_acc}')
     plt.savefig(plotsdir / f'{tag}_val_cm.png')
     df_val_pp['prediction'] = val_pred
     df_val_pp['predicted_letter'] = df_val_pp.prediction.map(class_encoding_dict)
     df_val_pp.reset_index(inplace=True)
     for ind_img in range(df_val_pp.shape[0]):
-        for ind_letter,l in enumerate(class_encoding_revers):
-            df_val_pp.loc[ind_img,'raw_pred_'+l] = val_pred_raw[ind_img,ind_letter]
+        for ind_letter, l in enumerate(class_encoding_revers):
+            df_val_pp.loc[ind_img, 'raw_pred_' + l] = val_pred_raw[ind_img, ind_letter]
     df_val_pp.to_csv(tagdir / f'val_res.csv')
 
     tst_pred_raw = model.predict(tst_gen)
@@ -164,22 +180,22 @@ if __name__ == '__main__':
     ax = fig.add_subplot(111)
     sns.heatmap(conf_mat, annot=True, ax=ax)
     plt.title(f'acc={tst_acc}')
-    ax.set_xticklabels(ticks_list,rotation=45)
-    ax.set_yticklabels(ticks_list,rotation=0)
-    ax.tick_params(axis='both', which='major',labelsize=10)
+    ax.set_xticklabels(ticks_list, rotation=45)
+    ax.set_yticklabels(ticks_list, rotation=0)
+    ax.tick_params(axis='both', which='major', labelsize=10)
     plt.savefig(plotsdir / f'{tag}_tst_cm.png')
     df_tst_pp['prediction'] = tst_pred
     df_tst_pp['predicted_letter'] = df_tst_pp.prediction.map(class_encoding_dict)
     df_tst_pp.reset_index(inplace=True)
     for ind_img in range(df_tst_pp.shape[0]):
-        for ind_letter,l in enumerate(class_encoding_revers):
-            df_tst_pp.loc[ind_img,'raw_pred_'+l] = tst_pred_raw[ind_img,ind_letter]
+        for ind_letter, l in enumerate(class_encoding_revers):
+            df_tst_pp.loc[ind_img, 'raw_pred_' + l] = tst_pred_raw[ind_img, ind_letter]
     df_tst_pp.to_csv(tagdir / f'tst_res.csv')
 
-#######################3
+    #######################
     tst_pred_raw = model.predict(tst_2_gen)
     tst_pred = tst_pred_raw.argmax(axis=1)
-    xticks_new_list =  [str(ii)+'('+jj+')' for ii,jj in class_encoding_dict.items() if ii in tst_pred]
+    xticks_new_list = [str(ii) + '(' + jj + ')' for ii, jj in class_encoding_dict.items() if ii in tst_pred]
     tst_labels = np.array([ii[0] for ii in df_tst_2_pp.class_encoding])
     tst_acc = sum(tst_pred == tst_labels) / len(tst_pred)
 
